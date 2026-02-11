c674b00226689c36e657fa1bff2352cc2c6f02ea
bernat <bernatbf11gmail.com>
2026-02-11 19:01:32 +0100

    add scheduler

---
+++ utils/scrapers_scheduler.py
@@ -0,0 +1,93 @@
+import os
+import sys
+from datetime import datetime, timedelta
+from supabase import create_client, Client
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from config import SUPABASE_URL, SUPABASE_KEY
+
+
+class ScrapersScheduler:
+    def __init__(self):
+        if not SUPABASE_URL or not SUPABASE_KEY:
+            raise ValueError("❌ Credentials for Supabase are missing in config.py or environment.")
+        self.client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+        self.scheduled_tasks = []
+
+    def run(self):
+        """
+        To run one time at the beginning of each season to schedule the scrapers for the upcoming tournaments.
+        For each tournament, schedule the corresponding scraper to run at the appropriate time.
+        Players should run 3 days before the tournament starts, matches should run 3 days after the tournament
+        and tournaments should run one day before it starts.
+        
+        :param self: Description
+        """
+        print("🚀 STARTING Scrapers Scheduler")
+        print("===============================")
+        tournaments = self._get_upcoming_tournaments()
+        for tournament in tournaments:
+            tournament_id = tournament['id']
+            start_date_str = tournament['start_date']
+            end_date_str = tournament['end_date']
+
+            start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+            now = datetime.now()
+
+            # Schedule Players Scraper 3 days before the tournament starts
+            players_scraper_time = start_date - timedelta(days=3)
+            if players_scraper_time > now:
+                self.scheduled_tasks.append({
+                    "task_type": "players",
+                    "scheduled_date": players_scraper_time,
+                    "tournament_id": tournament_id,
+                    "log": None
+
+                })
+
+            # Schedule Matches Scraper 3 days after the tournament ends
+            matches_scraper_time = end_date + timedelta(days=3)
+            if matches_scraper_time > now:
+                self.scheduled_tasks.append({
+                    "task_type": "matches",
+                    "scheduled_date": matches_scraper_time,
+                    "tournament_id": tournament_id,
+                    "log": None
+                })
+
+            # Schedule Tournament Scraper 1 day before the tournament starts
+            tournament_scraper_time = start_date - timedelta(days=1)
+            if tournament_scraper_time > now:
+                self.scheduled_tasks.append({
+                    "task_type": "tournament",
+                    "scheduled_date": tournament_scraper_time,
+                    "tournament_id": tournament_id,
+                    "log": None
+                })
+
+    def _get_upcoming_tournaments(self):
+        """
+        Fetch tournaments that have status != 'finished'. Plus we check 
+        the end date to avoid scheduling scrapers for tournaments that 
+        have already ended but haven't been marked as finished in the database.
+
+        :return: List of upcoming tournaments
+        """
+        return self.client.table('tournaments').select('*').neq('status', 'finished').lt('end_date', datetime.now().strftime("%Y-%m-%d")).execute()
+    
+
+    def _save_scheduled_tasks(self):
+        """
+        Save the scheduled tasks to the database.
+        """
+        if not self.scheduled_tasks:
+            print("⚠️ No tasks to schedule.")
+            return
+        try:
+            self.client.table("scheduled_tasks").upsert(self.scheduled_tasks, on_conflict="type, scheduled_date", ignore_duplicates=True).execute()
+            print("   ✅ Scheduled tasks saved to database.")
+        except Exception as e:
+            print(f"   ❌ Critical Error in saving scheduled tasks: {e}")
+            
+        
